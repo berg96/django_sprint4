eefb5960b5c4c066d6bb8fcc60c2190fb0e3ee8e
Artem Kulikov <chigar2010@yandex.ru>
2023-10-04 12:38:00 +0200

    add 403

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -142,3 +142,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_FAILURE_VIEW = 'pages.views.csrf_failure'
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -13,3 +13,7 @@ def rules(request):
 
 def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
