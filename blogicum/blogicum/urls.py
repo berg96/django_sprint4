0dfe1ea63101fe8bff009480167cb1385f527151
Artem Kulikov <chigar2010@yandex.ru>
2023-10-04 21:34:47 +0200

    add auth

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
 
 handler404 = 'pages.views.page_not_found'
