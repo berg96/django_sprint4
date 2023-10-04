3899ec90a0c7d39ea073933946d8f2ec534d630f
Artem Kulikov <chigar2010@yandex.ru>
2023-10-04 21:59:36 +0200

    add registration

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,14 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
