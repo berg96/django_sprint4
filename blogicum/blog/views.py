68750a79ca77e09f8d0313c633d95469b75fc490
Artem Kulikov <chigar2010@yandex.ru>
2023-10-07 18:22:21 +0200

    return

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -120,7 +120,7 @@ class PostUpdateView(PostMixin, LoginRequiredMixin, UpdateView):
         return reverse('blog:post_detail', kwargs={'pk': self.object.id})
 
 
-class PostDeleteView(LoginRequiredMixin, PostMixin, DeleteView):
+class PostDeleteView(PostMixin, LoginRequiredMixin, DeleteView):
     template_name = 'blog/create.html'
     pk_url_kwarg = 'post_id'
     success_url = reverse_lazy('blog:index')
@@ -128,12 +128,12 @@ class PostDeleteView(LoginRequiredMixin, PostMixin, DeleteView):
     def dispatch(
         self, request: http.HttpRequest, *args: Any, **kwargs: Any
     ) -> http.HttpResponse:
-        # if request.user.is_authenticated:
-        self.note = get_object_or_404(
-            Post, pk=kwargs['post_id'], author=request.user
-        )
-        # else:
-        #     return redirect('login')
+        if request.user.is_authenticated:
+            self.note = get_object_or_404(
+                Post, pk=kwargs['post_id'], author=request.user
+            )
+        else:
+            return redirect('login')
         return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
